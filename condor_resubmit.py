f2ed4c9c19b1198c4208b94815da61ac57ad904f
ram1123 <ramkrishna.sharma71@gmail.com>
2020-05-09 08:40:38 -0500

    updated condor resubmit script

--- condor_resubmit.py
+++ condor_resubmit.py
@@ -2,35 +2,34 @@ import os
 #Check which jobs failed
 
 string_to_search = "preselected entries from root:"
-path = "condor_logs/200405_072913"
-condor_file_name = "submit_condor_jobs_lnujj"
-Resubmission_txt = "_resubmit_2"
+path = "condor_logs/Run2018_v6_3May/200503_031434/"
+condor_file_name = "submit_condor_jobs_lnujj_v6_Run2018_v6_3May"
+Resubmission_txt = "1"
 
-print 'grep -L  "'+string_to_search+'" '+ path + os.sep +'*.stdout'
-output = os.popen('grep -L  "'+string_to_search+'" '+ path + os.sep +'*.stdout').read()
+grepCommand = 'grep -L  "'+string_to_search+'" '+ path + os.sep +'*.stdout'
+grepCommand = grepCommand.replace('//','/')
+print 'grep command: ',grepCommand
+output = os.popen(grepCommand).read()
 
-outjdl_file = open(condor_file_name+Resubmission_txt+".jdl","w")
+print('output:')
+# print output
+outjdl_file = open(condor_file_name+'_resubmit_'+Resubmission_txt+".jdl","w")
 with open(condor_file_name+".jdl") as myfile:
     head = [next(myfile) for x in xrange(7)]
-print head
+# print head
 
 for lines in head:
-  #outjdl_file.write(lines.replace(condor_file_name,condor_file_name+Resubmission_txt))
+  outjdl_file.write(lines)
+
 for lines in output.split():
-  #print "="*51,"\n\n"
-  #print lines.split('/')[2]
-  #print "==> ",lines.strip()
+  print "==> ",lines.strip()
   grep_output = os.popen('grep -E "Running.*root" '+lines.strip()).read()
-  print grep_output
-  #print "==> ",grep_output.split()[5].split('/')[-1]
-  #root_file = grep_output.split()[5].split('/')[-1]
-  #grep -A1 -B3 "034FD9B9-C376-2D4D-8BD8-B8848A1CB118.root" submit_condor_jobs_lnujj.jdl
-  #print "---"
-  #grep_condor_jdl_part = os.popen('grep -A1 -B3 "'+root_file+'" '+condor_file_name+'.jdl').read()
-  #print grep_condor_jdl_part.replace('Process)','Process)'+Resubmission_txt)
-  #outjdl_file.write(grep_condor_jdl_part.replace('Process)','Process)'+Resubmission_txt))
-  #for grep_lines in grep_condor_jdl_part.split():
-  #  print grep_lines.replace('Process)','Process)_resubmit_1')
-  #print "---"
-#outjdl_file.close()
-#os.system('cp '+condor_file_name+'.sh  '+condor_file_name+Resubmission_txt+'.sh')
+  # print grep_output
+  root_file = grep_output.split()[5].split('/')[-1]
+  grepCommand_GetJdlInfo = 'grep -A1 -B3 "'+root_file+'" '+condor_file_name+'.jdl'
+  # print grepCommand_GetJdlInfo
+  grep_condor_jdl_part = os.popen(grepCommand_GetJdlInfo).read()
+  updateString = grep_condor_jdl_part.replace('Process)','Process)'+ '_resubmit_' +Resubmission_txt)
+  # print updateString
+  outjdl_file.write(updateString)
+outjdl_file.close()
